c7209950ea6f9fd3e20630267992056057a13d5f
Cameron Daniel <55892704+czdaniel12@users.noreply.github.com>
2024-07-23 09:30:21 -0700

    Added return values for user first and last names - will reevaluate

--- Python/Vehicles/maintenance.py
+++ Python/Vehicles/maintenance.py
@@ -7,8 +7,8 @@ import sys
 root_dir = os.path.abspath('..')
 sys.path.append(root_dir)
 import cfg_loader
+import sql_loader
 import user_utils as u
-from sql_loader import LoadSQL
 
 # Config identifiers
 script_key = 'maintenance'
@@ -17,19 +17,33 @@ use_database = True
 # Get SQL db name, establish connection and set cursor
 if use_database:
     db_name = cfg_loader.get_db(script_key)
-    sql = LoadSQL(db_name)
+    sql = sql_loader.LoadSQL(db_name)
 
+# Evaluate if this needs a function or just call from main()
+def check_user():
+    uid, fname, lname = u.main(db_name)
+
+    result = sql.query('SELECT FirstName, LastName FROM users WHERE Id=?', (uid,))
+    for fname, lname in result:
+        print(f'Welcome back, {fname} {lname}!')
+    
+
+def new_record(uid):
+    pass
+
+def find_record(uid):
+    print('Find existing records:')
 
 
 def main():
-    uid = u.main()
+    pass
     
 if __name__ == "__main__":
     main()
 
 
 '''
-    result = sql.query('select FirstName, LastName from users where id=?', (uid,))
+    result = sql.query('select FirstName, LastName from users where Id=?', (uid,))
     for fname, lname in result:
         print(f'It\'s {fname} {lname}')
 '''
